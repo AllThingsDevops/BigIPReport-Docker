4b707dd1792b8432f4c8b3f4a70b79766427c5ef
Patrik Jonsson <patrik.jonsson@netent.com>
2021-04-14 19:32:45 +0200

    Adding wrapper script to handle secret injection into the config file

---
+++ bigipreport-k8s/run.ps1
@@ -0,0 +1,23 @@
+if (Test-path "/opt/bigipreport/config/bigipreportconfig.xml") {
+    $Config = Get-Content "/opt/bigipreport/config/bigipreportconfig.xml"
+    
+    $F5_USERNAME = $env:F5_USERNAME
+    $F5_PASSWORD = $env:F5_PASSWORD
+    $F5_SUPPORT_USERNAME = $env:F5_SUPPORT_USERNAME
+    $F5_SUPPORT_PASSWORD = $env:F5_SUPPORT_USERNAME
+
+    if ($null -notin @($F5_USERNAME, $F5_PASSWORD, $F5_SUPPORT_USERNAME, $F5_SUPPORT_PASSWORD)){
+        Write-Host "Environment variables set, replacing credentials in the configmap"
+        $Config -replace '{{F5_USERNAME}}', $F5_USERNAME `
+                -replace '{{F5_PASSWORD}}', $F5_PASSWORD `
+                -replace '{{F5_SUPPORT_USERNAME}}', $F5_SUPPORT_USERNAME `
+                -replace '{{F5_SUPPORT_PASSWORD}}', $F5_SUPPORT_PASSWORD `
+                | Out-File /opt/bigipreport/config/bigipreportconfig.xml
+    } Else {
+        Write-Host "At least one environment variable missing, using config map as is"
+    }
+} else {
+    Write-Error "Config file was not found in /opt/bigipreport/config/bigipreportconfig.xml, have you mounted the config map?"
+}
+
+& pwsh -c /opt/bigipreport/bigipreport.ps1 /opt/bigipreport/config/bigipreportconfig.xml
